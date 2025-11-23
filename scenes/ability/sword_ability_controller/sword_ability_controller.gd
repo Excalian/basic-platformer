a4f87eeb6f26f449de50a20e73fc916c4c34b23a
Marcellino Abdelmalak <me@excalian.com>
2025-11-22 16:09:06 -0800

    started workin on the sword ability

---
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var sword_ability: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.timeout.connect(on_timer_timeout) # shorthand for get_node("Timer").timeout
+
+
+func on_timer_timeout():
+	var player: Node2D = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	var sword_instance: Node2D = sword_ability.instantiate() as Node2D
+	player.get_parent().add_child(sword_instance)
+	
+	sword_instance.global_position = player.global_position
